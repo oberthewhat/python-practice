3fe774a8afa604fa8e1d02cdb9945df089fc429e
John Obertubbesing <oberthewhat@gmail.com>
2020-06-16 18:52:25 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+print("hello world")
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+
+is_patient = True
+age = 35
+name ='bobby'
+
+
+
+name = input('what is your first name? ')
+print('hi', name)
